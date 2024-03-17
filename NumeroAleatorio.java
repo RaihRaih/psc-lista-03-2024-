d62abc5464525581c02e8fa8a05c588ee5079f25
Raissa Rodrigues <105363135+RaihRaih@users.noreply.github.com>
2024-03-17 17:13:16 -0300

    Questão 6

--- NumeroAleatorio.java
+++ NumeroAleatorio.java
@@ -1,8 +1,8 @@
 /*Raissa Rodrigues - Sistemas de Informação
 * 6. Crie um algoritmo que leia dois números inteiros e realize o sorteio de um número aleatório. O algoritmo deve validar qual é o 
 menor e o maior número informado pelo teclado, para que independente da ordem que o usuário digite os números, consiga realizar o sorteio.
-Se o algoritmo gerar um número par, escreva na tela o número gerado e informe que ele é par. Se o algoritmo gerar um número ímpar, 
-escreva na tela o número gerado e que ele é um número ímpar.*/
+Se o algoritmo gerar um número par, escreva na tela o número gerado e informe que ele é par. Se o algoritmo gerar um número ímpar, escreva na tela
+o número gerado e que ele é um número ímpar.*/
 
 import java.util.Random;
 import java.util.Scanner;
